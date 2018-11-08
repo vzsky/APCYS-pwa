97f9270ad2c9ea9d93443b4e43c23bb394229a7f
my99N <talay@10ninox.com>
2018-11-08 19:19:37 +0700

    Secretify

--- loginapi.py
+++ loginapi.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 mysql = MySQL()
 
 app.config['SECRET_KEY'] = 'secret'
-app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = ''
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'APCYS'
-app.config['MYSQL_DATABASE_HOST'] = '10.205.240.221'
+app.config['MYSQL_DATABASE_DB'] = ''
+app.config['MYSQL_DATABASE_HOST'] = ''
 
 #### APP MYSQL CONFIG
 
