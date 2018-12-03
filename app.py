4b7abf28f52344eb49acfabc9989db390f754258
VZSKY <talay@10ninox.com>
2018-12-03 13:55:10 +0700

    debug false

--- app.py
+++ app.py
@@ -298,4 +298,4 @@ def boot():
 	return render_template('boot.html')
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=port ,debug = True)
+	app.run(host='0.0.0.0', port=port ,debug = False)
